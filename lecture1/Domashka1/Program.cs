be15eb59c24280bcc0878cb5e5877afc1120fbfc
Angelina <vita97svobodnya@gmail.com>
2024-03-07 09:12:45 +0900

    added lecture1: Domashka1

---
+++ lecture1/Domashka1/Program.cs
@@ -0,0 +1,15 @@
+﻿int i1 = 7;
+int i2 = 4;
+int product1 = i1 * i2;
+Console.WriteLine(product1);
+
+long  l1 = 9;
+double d1 = 4.2;
+double sum1 = l1 + d1;
+Console.WriteLine(sum1);
+
+
+double quotient1 = product1 / sum1;
+Console.WriteLine(quotient1);
+
+
