42dc00575da452cb4da97bfd9262dd801207c29f
Angelina <vita97svobodnya@gmail.com>
2024-03-07 09:54:18 +0900

    Task4

---
+++ lecture1/Task4/Program.cs
@@ -0,0 +1,26 @@
+﻿int a = 6;
+int b = 7;
+int c = 9;
+int d = 4;
+int e = 10;
+
+int max = a;
+
+if (b > max)
+{
+    b = max;
+}
+if (c > max)
+{
+    c = max;
+}
+if (d > max)
+{
+    d = max;
+}
+if (e > max)
+{
+    e = max;
+}
+Console.WriteLine(max);
+
